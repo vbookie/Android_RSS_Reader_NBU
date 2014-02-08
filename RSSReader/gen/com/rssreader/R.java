5aad7ad5268c45f3bb6d73b83c632988e43ee374
vbookie <vbookie@gmail.com>
2014-02-08 10:29:18 +0200

    Added summary percentage preference.

--- RSSReader/gen/com/rssreader/R.java
+++ RSSReader/gen/com/rssreader/R.java
@@ -11,6 +11,8 @@ public final class R {
     public static final class array {
         public static final int pref_refresh_rate_intervals_values=0x7f060000;
         public static final int pref_refresh_rate_intevals_titles=0x7f060001;
+        public static final int pref_summary_perc_intervals_titles=0x7f060003;
+        public static final int pref_summary_perc_intervals_values=0x7f060002;
     }
     public static final class attr {
     }
@@ -91,36 +93,39 @@ public final class R {
         public static final int settings_feed_list=0x7f090003;
     }
     public static final class string {
-        public static final int action_new_feed_url=0x7f070010;
-        public static final int action_refresh=0x7f070007;
-        public static final int action_settings=0x7f070006;
-        public static final int app_name=0x7f070001;
-        public static final int button_cancel=0x7f07000a;
-        public static final int button_ok=0x7f070008;
-        public static final int button_save=0x7f070009;
-        public static final int pref_feeds_key=0x7f07000e;
-        public static final int pref_feeds_title=0x7f07000d;
+        public static final int action_new_feed_url=0x7f070013;
+        public static final int action_refresh=0x7f070008;
+        public static final int action_settings=0x7f070007;
+        public static final int app_name=0x7f070002;
+        public static final int button_cancel=0x7f07000b;
+        public static final int button_ok=0x7f070009;
+        public static final int button_save=0x7f07000a;
+        public static final int pref_feeds_key=0x7f07000f;
+        public static final int pref_feeds_title=0x7f07000e;
         public static final int pref_refresh_rate_interval_default=0x7f070000;
-        public static final int pref_refresh_rate_key=0x7f07000c;
-        public static final int pref_refresh_rate_title=0x7f07000b;
-        public static final int settings_feed_alert_duplicate=0x7f07001b;
-        public static final int settings_feed_alert_empty=0x7f070019;
-        public static final int settings_feed_alert_invalid=0x7f07001a;
-        public static final int settings_feed_delete_action=0x7f070018;
-        public static final int settings_feed_edit_action=0x7f070017;
-        public static final int settings_no_feeds=0x7f07001c;
-        public static final int settings_single_feed_default_url=0x7f070015;
-        public static final int settings_single_feed_description_add=0x7f070013;
-        public static final int settings_single_feed_description_edit=0x7f070014;
-        public static final int settings_single_feed_empty_url=0x7f07001d;
-        public static final int settings_single_feed_url_title=0x7f070016;
-        public static final int title_activity_base=0x7f070002;
-        public static final int title_activity_settings=0x7f070003;
-        public static final int title_activity_settings_edit_feed=0x7f070012;
-        public static final int title_activity_settings_feed_list=0x7f07000f;
-        public static final int title_activity_settings_new_feed=0x7f070011;
-        public static final int title_feeditem_detail=0x7f070005;
-        public static final int title_feeditem_list=0x7f070004;
+        public static final int pref_refresh_rate_key=0x7f07000d;
+        public static final int pref_refresh_rate_title=0x7f07000c;
+        public static final int pref_summary_perc_interval_default=0x7f070001;
+        public static final int pref_summary_perc_key=0x7f070011;
+        public static final int pref_summary_perc_title=0x7f070010;
+        public static final int settings_feed_alert_duplicate=0x7f07001e;
+        public static final int settings_feed_alert_empty=0x7f07001c;
+        public static final int settings_feed_alert_invalid=0x7f07001d;
+        public static final int settings_feed_delete_action=0x7f07001b;
+        public static final int settings_feed_edit_action=0x7f07001a;
+        public static final int settings_no_feeds=0x7f07001f;
+        public static final int settings_single_feed_default_url=0x7f070018;
+        public static final int settings_single_feed_description_add=0x7f070016;
+        public static final int settings_single_feed_description_edit=0x7f070017;
+        public static final int settings_single_feed_empty_url=0x7f070020;
+        public static final int settings_single_feed_url_title=0x7f070019;
+        public static final int title_activity_base=0x7f070003;
+        public static final int title_activity_settings=0x7f070004;
+        public static final int title_activity_settings_edit_feed=0x7f070015;
+        public static final int title_activity_settings_feed_list=0x7f070012;
+        public static final int title_activity_settings_new_feed=0x7f070014;
+        public static final int title_feeditem_detail=0x7f070006;
+        public static final int title_feeditem_list=0x7f070005;
     }
     public static final class style {
         /** 
